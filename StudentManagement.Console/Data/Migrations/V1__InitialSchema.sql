b8e8595009161c8fd89dbace601ec5cce1166a43
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2025-10-02 23:19:18 +0200

    Convert init state to pure state-based SQL

--- StudentManagement.Console/Data/Migrations/V1__InitialSchema.sql
+++ StudentManagement.Console/Data/Migrations/V1__InitialSchema.sql
@@ -1,14 +1,12 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
+﻿CREATE TABLE [Students] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [EnrollmentDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+);
 
-BEGIN TRANSACTION;
 CREATE TABLE [Courses] (
     [Id] int NOT NULL IDENTITY,
     [Title] nvarchar(max) NOT NULL,
@@ -21,21 +19,7 @@ CREATE TABLE [Enrollments] (
     [StudentId] int NOT NULL,
     [CourseId] int NOT NULL,
     [Grade] nvarchar(max) NOT NULL,
-    CONSTRAINT [PK_Enrollments] PRIMARY KEY ([Id])
+    CONSTRAINT [PK_Enrollments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Enrollments_Students] FOREIGN KEY ([StudentId]) REFERENCES [Students]([Id]),
+    CONSTRAINT [FK_Enrollments_Courses] FOREIGN KEY ([CourseId]) REFERENCES [Courses]([Id])
 );
-
-CREATE TABLE [Students] (
-    [Id] int NOT NULL IDENTITY,
-    [FirstName] nvarchar(max) NOT NULL,
-    [LastName] nvarchar(max) NOT NULL,
-    [Email] nvarchar(max) NOT NULL,
-    [EnrollmentDate] datetime2 NOT NULL,
-    CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
-);
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20251002130816_InitialSchema', N'9.0.9');
-
-COMMIT;
-GO
-
