def64c084ebdd96c3006fdc5aebb83ae99c5890f
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2025-10-02 22:24:44 +0200

    Changed Credits from int to decimal(5,2)
    Generated migration artifact: `Data/Migrations/V7__ModifyCreditsDataType.sql`

---
+++ StudentManagement.Console/Data/Migrations/20251002202052_ModifyCreditsDataType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagement.Console.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyCreditsDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ StudentManagement.Console/Data/Migrations/V7__ModifyCreditsDataType.sql
@@ -0,0 +1,7 @@
+﻿BEGIN TRANSACTION;
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251002202052_ModifyCreditsDataType', N'9.0.9');
+
+COMMIT;
+GO
+
