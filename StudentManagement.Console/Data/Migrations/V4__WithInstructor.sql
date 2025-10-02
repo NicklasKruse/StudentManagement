275065d92b2e25d0326d74853000d8048233774d
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2025-10-02 23:32:20 +0200

    .

--- StudentManagement.Console/Data/Migrations/V4__WithInstructor.sql
+++ StudentManagement.Console/Data/Migrations/V4__WithInstructor.sql
@@ -9,11 +9,22 @@
     CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
 );
 
+CREATE TABLE [Instructors] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [HireDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Instructors] PRIMARY KEY ([Id])
+);
+
 CREATE TABLE [Courses] (
     [Id] int NOT NULL IDENTITY,
     [Title] nvarchar(max) NOT NULL,
     [Credits] int NOT NULL,
-    CONSTRAINT [PK_Courses] PRIMARY KEY ([Id])
+    [InstructorId] int NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Courses_Instructors] FOREIGN KEY ([InstructorId]) REFERENCES [Instructors]([Id])
 );
 
 CREATE TABLE [Enrollments] (
